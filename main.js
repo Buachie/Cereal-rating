ef1d8f21a35dc0f98061b806b8e358d92e110328
Aaron Buachie <aaron.buachiea@gmail.com>
2020-04-06 12:10:07 -0400

    Added rating button, button now opens modal displaying approval, clicking background closes modal,

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 const cereal = [
   {
     name: "Apple Cinnamon Cheerios",
+
     type: "Cold",
     calories: "110",
     vitamins: "25",
     sugar: "10",
-    rating: "29.5"
+    rating: 29.5,
   },
   {
     name: "Apple Jacks",
@@ -13,7 +14,7 @@ const cereal = [
     calories: "110",
     vitamins: "25",
     sugar: "14",
-    rating: "33.1"
+    rating: 33.1,
   },
   {
     name: "Cap'n'Crunch",
@@ -21,7 +22,7 @@ const cereal = [
     calories: "120",
     vitamins: "25",
     sugar: "12",
-    rating: "18"
+    rating: 18,
   },
   {
     name: "Cheerios",
@@ -29,7 +30,7 @@ const cereal = [
     calories: "110",
     vitamins: "25",
     sugar: "1",
-    rating: "50.8"
+    rating: 50.8,
   },
   {
     name: "Cinnamon Toast Crunch",
@@ -37,7 +38,7 @@ const cereal = [
     calories: "120",
     vitamins: "25",
     sugar: "9",
-    rating: "19.8"
+    rating: 19.8,
   },
   {
     name: "Cocoa Puffs",
@@ -45,7 +46,7 @@ const cereal = [
     calories: "110",
     vitamins: "25",
     sugar: "13",
-    rating: "22.7"
+    rating: 22.7,
   },
   {
     name: "Cream of Wheat (Quick)",
@@ -53,7 +54,7 @@ const cereal = [
     calories: "100",
     vitamins: "0",
     sugar: "0",
-    rating: "64.5"
+    rating: 64.5,
   },
   {
     name: "Froot Loops",
@@ -61,7 +62,7 @@ const cereal = [
     calories: "110",
     vitamins: "25",
     sugar: "13",
-    rating: "32.2"
+    rating: 32.2,
   },
   {
     name: "Frosted Flakes",
@@ -69,7 +70,7 @@ const cereal = [
     calories: "110",
     vitamins: "25",
     sugar: "11",
-    rating: "31.4"
+    rating: 31.4,
   },
   {
     name: "Fruity Pebbles",
@@ -77,7 +78,7 @@ const cereal = [
     calories: "110",
     vitamins: "25",
     sugar: "12",
-    rating: "28"
+    rating: 28,
   },
   {
     name: "Honey Nut Cheerios",
@@ -85,7 +86,7 @@ const cereal = [
     calories: "110",
     vitamins: "25",
     sugar: "10",
-    rating: "31"
+    rating: 31,
   },
   {
     name: "Lucky charms",
@@ -93,7 +94,7 @@ const cereal = [
     calories: "110",
     vitamins: "25",
     sugar: "12",
-    rating: "26"
+    rating: 26,
   },
   {
     name: "Multi-Grain Cheerios",
@@ -101,7 +102,7 @@ const cereal = [
     calories: "100",
     vitamins: "25",
     sugar: "6",
-    rating: "40"
+    rating: 40,
   },
   {
     name: "Quaker Oatmeal",
@@ -109,7 +110,7 @@ const cereal = [
     calories: "100",
     vitamins: "0",
     sugar: "-1",
-    rating: "50.8"
+    rating: 50.8,
   },
   {
     name: "Raisin Bran",
@@ -117,7 +118,7 @@ const cereal = [
     calories: "120",
     vitamins: "25",
     sugar: "12",
-    rating: "39.2"
+    rating: 39.2,
   },
   {
     name: "Rice Krispies",
@@ -125,7 +126,7 @@ const cereal = [
     calories: "110",
     vitamins: "25",
     sugar: "3",
-    rating: "40.5"
+    rating: 40.5,
   },
   {
     name: "Trix",
@@ -133,13 +134,14 @@ const cereal = [
     calories: "110",
     vitamins: "25",
     sugar: "12",
-    rating: "27.7"
-  }
+    rating: 27.7,
+  },
 ];
 
 function searchCereal(cerealName) {
-  cereal.forEach(function(element) {
+  cereal.forEach(function (element) {
     if (element.name.includes(document.getElementById("search").value)) {
+      let body = $("</body>");
       let cerealItem = $("<div/>", { class: "cereal-item" });
       let cerealName = $("<div/>", { class: "cereal-name" }).html(element.name);
       let cerealType = $("<div/>", { class: "cereal-type" }).html(
@@ -157,6 +159,9 @@ function searchCereal(cerealName) {
       let cerealRating = $("<div/>", { class: "cereal-rating" }).html(
         "Rating: " + element.rating + "/100"
       );
+      let approvalButton = $("<button>", {
+        class: "approval-button",
+      }).html("Check Approval");
 
       $("#result").append(cerealItem);
 
@@ -166,6 +171,45 @@ function searchCereal(cerealName) {
       $(cerealVitamins).appendTo(cerealItem);
       $(cerealSugar).appendTo(cerealItem);
       $(cerealRating).appendTo(cerealItem);
+      $(approvalButton).appendTo(cerealItem);
+
+      $(approvalButton).click(function () {
+        let modalBackground = $("<div/>", { class: "modal-background" });
+        let modal = $("<div/>", {
+          class: "modal",
+        });
+        let approvedImg = $("<img>", {
+          class: "approval-img",
+          src:
+            "https://cdn.pixabay.com/photo/2017/06/09/05/21/ok-2385794_960_720.jpg",
+        });
+        let deniedImg = $("<img>", {
+          class: "approval-img",
+          src:
+            "https://cdn.pixabay.com/photo/2018/01/12/21/06/shocked-3079071_960_720.jpg",
+        });
+        let approvedTxt = $("<h2/>").html("This cereal is Mom Approved!");
+        let deniedTxt = $("<h2/>").html("This cereal is Mom Denied!");
+
+        if (element.rating > 35) {
+          $(modalBackground).appendTo("body");
+          $(modal).appendTo(modalBackground);
+          $(approvedImg).appendTo(modal);
+          $(approvedTxt).appendTo(modal);
+        } else {
+          $(modalBackground).appendTo("body");
+          $(modal).appendTo(modalBackground);
+          $(deniedImg).appendTo(modal);
+          $(deniedTxt).appendTo(modal);
+        }
+
+        $(modalBackground).click(function () {
+          $(modalBackground).hide();
+          $(modal).hide();
+          $(approvedImg).hide();
+          $(approvedTxt).hide();
+        });
+      });
     }
   });
 }
